fd627937db67151448847b3a92b0f4e4ac3a565a
ahmad-masud <ahmadmasud25@hotmail.com>
2024-11-19 18:15:07 -0800

    Added LeetCode 88

---
+++ solutions/88/solution1/code.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        res = []
+        p1 = p2 = 0
+
+        while p1 < m and p2 < n:
+            if nums1[p1] < nums2[p2]:
+                res.append(nums1[p1])
+                p1 += 1
+            else:
+                res.append(nums2[p2])
+                p2 += 1
+
+        while p1 < m:
+            res.append(nums1[p1])
+            p1 += 1
+
+        while p2 < n:
+            res.append(nums2[p2])
+            p2 += 1
+
+        nums1[:m + n] = res
