581478da98d4bc49d4ec5798d5d51ca2110a11d0
ahmad-masud <ahmadmasud25@hotmail.com>
2024-11-24 14:16:17 -0800

    Added LeetCode 1046

---
+++ solutions/1046/solution1/code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            max1 = max(stones)
+            stones.remove(max1)
+
+            max2 = max(stones)
+            stones.remove(max2)
+        
+            if max1 != max2:
+                stones.append(max1 - max2)
+    
+        return stones[0] if stones else 0
